1637e44cbcb5da718a3bf7d04c091c80edda04b8
Mrebb <madhu.rebbana@gmail.com>
2018-04-21 20:02:56 -0700

    hamburger menu, styling for new page

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,14 +1,14 @@
 'use strict';
+// client-side routes
 if (window.location.protocol.startsWith('https:')) {
   page.base('/books');
 }
-// This is where we define the client-side routes
 //routes for everything except valid routes
 page('/*', (ctx,next) => {
   $('.container').hide()
   next();
 })
-
+//Main routes
 page('/',(ctx) => app.Book.fetchAll(ctx).then(app.bookView.initIndexPage));
 page('/books/new',app.bookView.initNewPage);
 page('/books/:id',(ctx) => app.Book.fetchOne(ctx.params.id).then(app.bookView.initDetailedPage));
